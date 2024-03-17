7103df9d820c6ca14ca563af414356eab6cb3242
Neto sergey test <serez.gavriliv@mail.ru>
2024-03-17 12:24:05 +0300

    Initial Add

---
+++ src/main/java/ru/netology/repository/ShopRepository.java
@@ -0,0 +1,73 @@
+package ru.netology.repository;
+import ru.netology.NotFoundException;
+import ru.netology.Product;
+
+public class ShopRepository {
+    private Product[] products = new Product[0];
+
+    private Product[] addToArray(Product[] current, Product product) {
+        Product[] tmp = new Product[current.length + 1];
+        for (int i = 0; i < current.length; i++) {
+            tmp[i] = current[i];
+        }
+        tmp[tmp.length - 1] = product;
+        return tmp;
+    }
+
+    public void add(Product product) {
+        products = addToArray(products, product);
+    }
+
+    public Product[] findAll() {
+        return products;
+    }
+
+    // Этот способ мы рассматривали в теории в теме про композицию
+    public void remove(int id) {
+        Product[] tmp = new Product[products.length - 1];
+        int copyToIndex = 0;
+        for (Product product : products) {
+            if (product.getId() != id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+            }
+        }
+        products = tmp;
+    }
+    public void save(Product product) {
+        if (findById(product.getId()) != null) {
+            throw new ArithmeticException(
+                    "Element with id: " + product.getId() + " already exist"
+            );
+        }
+        products = addToArray(products, product);
+    }
+
+    public void removeById(int id) {
+        if (findById(id) == null) {
+            throw new NotFoundException(
+                    "Element with id: " + id + " not found"
+            );
+        }
+        Product[] tmp = new Product[products.length - 1];
+        int index = 0;
+        for (Product product : products) {
+            if (product.getId() != id) {
+                tmp[index] = product;
+                index++;
+            }
+        }
+        products = tmp;
+    }
+
+    public Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+
+}
